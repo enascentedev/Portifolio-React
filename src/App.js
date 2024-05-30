6d65f82706013544622874f58809d53436dbb5d1
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-05-29 21:02:53 -0300

    ajustando layout app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import './assets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portifolio Emanuel</h1>
-			<Header/>
-			<About/>
-			<Projects/>
-    </div>
+    <>
+      <div className="background"></div>
+      <div className="App">
+        <h1>Portifolio Emanuel</h1>
+        <Header />
+        <About />
+        <Projects />
+      </div>
+    </>
   );
 }
 
